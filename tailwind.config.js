52a5b995b05106fc59f2585fd08362a7465f9544
ankit-lighthouse <158129409+ankit-lighthouse@users.noreply.github.com>
2025-03-08 13:26:42 +0530

    formated docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./src/**/*.{html,js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
 
+  plugins: [],
+};
